204bea79f9f3109b54645e646e6c245e599ea7b2
Chris <flaeri@otterbro.com>
2022-11-29 20:53:43 +0000

    discoComp: Implement auto downscale based on res
    Automatic downscale based on the bitrate/duration. Implement bitrate
    guard if too low. Increase overhead safety to 10% (small files would
    go over). Small touch-ups.

--- discoCompress.ps1
+++ discoCompress.ps1
@@ -1,7 +1,7 @@
 ### User configurable ###
 
 # Size and Bitrate
-$maxSize = 50   #Megabytes, usually limit is 50 or 100 depending on the server/nitro
+$maxSize = 50  #Megabytes, usually limit is 8, 50 or 100 depending on the server/nitro
 $audioBr = 128  #Kilobytes, audio bitrate
 
 # Speed and Quality
@@ -10,7 +10,7 @@ $x264p = "veryfast" # x264 preset (slow, medium, fast, faster, veryfast)
 
 # Misc
 $suffix = "disc"    #output is tagged with this, like "myVideo-disc.webm/mp4"
-$ll = 24            #how much ffmpeg outputs to the console
+$ll = 32            #how much ffmpeg outputs to the console. 24 for quiet
 
 ### STOP TOUCHY NOW ###
 
@@ -28,6 +28,7 @@ write-host "`r"
 #static
 $question = "Fast (h264, lower quality), Slow (vp9, higher quality)"
 $option = "&Fast", "&Slow"
+$downscale = -2 #default placeholder in case of no scaling
 
 #pick encode, 0 = fast, 1 = slow
 $choice = $Host.UI.PromptForChoice("Slow or Fast?", $question, $Option, 1)
@@ -48,7 +49,7 @@ if ($choice -eq 0) {
         write-host "Nvenc OK!" -ForegroundColor green
         $enc = 0 #nvenc
     } else {
-        write-host "No nvenc device found! x264 it is!" -ForegroundColor Yellow
+        write-host "No nvenc device found, x264 it is!" -ForegroundColor Yellow
         $enc = 1 #x264
     }
     $ext = "mp4"
@@ -58,21 +59,48 @@ if ($choice -eq 0) {
     $ext = "webm"
 }
 
-# 5% overhead safety 
-$safeSize = $maxSize * 0.95
+# 10% overhead safety 
+$safeSize = $maxSize * 0.90
 
 ## get duration of file
 $durationSec = ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "$video"
 $durationSecClamp = [math]::Round($durationSec)
 
+#get width
+$vidHeight = ffprobe -v error -show_entries stream=height -of default=noprint_wrappers=1:nokey=1 "$video"
+
+# convert size
 $vidBr = $safeSize * 8 / $durationSec * 1000 # size in MB * 8 = bits, divided by duration. x 1000 for kbps
 $vidBr = [math]::Round($vidBr) # int plz
 $vidBr = $vidBr - $audioBr # account for audio bitrate
-$bufSize = $vidBr*2
+$bufSize = $vidBr*2 #bufsize x2 increases quality, lowers accuracy
 
-write-host "`n$name`:"
+# bitrate guard
+if ($vidBr -le 200) {
+    write-host "`nBitrate is too low ($vidBr kbps)! Either increase the filesize or shorten the duration" -ForegroundColor Red
+    write-host "`Exiting!" -ForegroundColor Red
+    psPause
+    exit
+}
+
+$bpw = $vidBr/$vidHeight #bitrate per video height
+if ($enc -eq 2) {$bpw = $bpw * 2} # if vp9, 2x bpw
+
+# used to be 5.5
+if ($bpw -lt 5.6) {
+    if ($vidHeight -ge 1440) {$downscale = 1080}
+    $bpw = $vidBr/$downscale
+    write-host "`nNot enough bit rate for $vidHeight`p, downscaling..." -ForegroundColor Yellow
+    if ($bpw -lt 5.5) {
+        $downscale = 720
+        write-host "Go to 720p" -ForegroundColor Yellow
+    }
+}
+
+write-host "`nFile: $name`:"
 write-host "Duration: $durationSecClamp sec" -ForegroundColor Yellow
-write-host "Bitrate (kbps): $vidBr" -ForegroundColor Yellow
+write-host "Bitrate: $vidBr kbps" -ForegroundColor Yellow
+Write-host "Max Size: $maxSize mb" -ForegroundColor Yellow
 write-host "`nGo? ctrl+c to cancel" -ForegroundColor Green
 pause
 
@@ -84,20 +112,20 @@ switch ( $enc )
     # h264 nvenc
     0 {ffmpeg -hide_banner -loglevel $ll -hwaccel cuda -i $video -c:v h264_nvenc -preset p6 -rc vbr `
         -cq 24 -bf 2 -maxrate $vidBr`k -bufsize $bufSize`k -spatial-aq 1 -temporal-aq 1 -aq-strength 7 `
-        -b:a $audioBr`k -pix_fmt yuv420p -movflags +faststart $dir\$baseName-$suffix.$ext}
+        -b:a $audioBr`k -pix_fmt yuv420p -vf scale=-2:$downscale -movflags +faststart $dir\$baseName-$suffix.$ext}
     # libx264
-    1 {ffmpeg -hide_banner -loglevel $ll -i $video -c:v libx264 -preset $x264p -crf 1 -b:v $vidBr`k -maxrate $vidBr`k -bufsize $bufSize`k `
-        -b:a $audioBr`k -pix_fmt yuv420p -movflags +faststart $dir\$baseName-$suffix.$ext}
+    1 {ffmpeg -hide_banner -loglevel $ll -i $video -c:v libx264 -preset $x264p -crf 20 -b:v $vidBr`k -maxrate $vidBr`k -bufsize $bufSize`k `
+        -b:a $audioBr`k -pix_fmt yuv420p -vf scale=-2:$downscale -movflags +faststart $dir\$baseName-$suffix.$ext}
     # vp9
-    2 {ffmpeg -hide_banner -loglevel $ll -i $video -c:v libvpx-vp9 -cpu-used $cpuUsed -row-mt 1 -crf 26 `
-        -b:v $vidBr`k -b:a $audioBr`k -pix_fmt yuv420p $dir\$baseName-$suffix.$ext}
+    2 {ffmpeg -hide_banner -loglevel $ll -i $video -c:v libvpx-vp9 -cpu-used $cpuUsed -row-mt 1 -crf 24 `
+        -b:v $vidBr`k -b:a $audioBr`k -pix_fmt yuv420p -vf scale=-2:$downscale $dir\$baseName-$suffix.$ext}
 }
 
 write-host "`n"
 Stop-Timer $name $startTime
 
 $outputFile = get-childitem -Path "$dir\$baseName-$suffix.$ext"
-$outputFileSize = [math]::Round($outputFile.Length / 1MB)
+$outputFileSize = [math]::Round($outputFile.Length / 1MB, 2)
 if ($outputFileSize -gt $maxSize) {
     write-host "Fail! File is larger ($outputFileSize MB) than $maxSize MB" -ForegroundColor Red
 } else {
