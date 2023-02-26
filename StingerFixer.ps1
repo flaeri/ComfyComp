a8c716e483db72f1743ab729b383349f5c13f3f2
Chris (Flaeri) <flaeri@otterbro.com>
2023-02-26 22:14:40 +0000

    StingerFixer: Rename and modernize
    Give a reasonable name. Move to drag and drop model. No config needed,
    just generate file where the source is.

--- StingerFixer.ps1
+++ StingerFixer.ps1
@@ -4,8 +4,10 @@ $dir = Split-Path $scriptPath
 Push-Location $dir
 
 #loading functions
+#load helpers
+. .\helpers\banner.ps1
+. .\helpers\ffmpegInfo.ps1
 . .\helpers\commonFunctions.ps1
-. .\helpers\Verifier.ps1
 
 #settings
 $ll = 24            #loglevel, set 32 if you want normal output. This (24) will only show warnings.
@@ -19,42 +21,38 @@ $suffix = "vp9fix"  #name that is used as a suffix for files in the output folde
 write-host "Stinger fixer. Re-encodes potentially wonky alpha videos" -ForegroundColor Magenta -BackgroundColor black
 write-host "`r"
 
-Push-Location -path $rootLocation #Don't edit edit this, edit the config.json or delete it
-
 write-host "`r"
 Write-Host "Please select the stinger file" -ForegroundColor Yellow
-Pause
 
-$inputStinger = Get-FileName
-if ($inputStinger -eq "") {
-    Write-Host "you didn't select anything, exiting" -ForegroundColor Red
-    psPause
-    exit
-}
+$video = read-host -Prompt "`nPlease drag&drop a video, then hit Enter" #drag video in
+$inputStinger = get-childitem -path ($video -replace '"', "") #input is dumbass string, fix it
+Clear-Host
 
-$Name = Get-ChildItem $inputStinger
-$shortName = $Name.BaseName
+# naming stuff
+Set-FileVars($inputStinger) #full=wPath, base=noExt,
+
 $inputCodec = ffprobe -v error -select_streams v:0 -show_entries stream=codec_name -of default=noprint_wrappers=1:nokey=1 $inputStinger
 
 write-host "Processing, please be patient" -ForegroundColor Yellow
 
+$outFile = "$dir\$baseName-$suffix.webm"
 $startTime = get-date
 
 if ($inputCodec -eq "vp9") {
     write-host "vp9, ensuring correct decoder" -ForegroundColor Yellow
     ffmpeg -$ow -loglevel $ll -c:v libvpx-vp9 -i "$inputStinger" `
     -c:v libvpx-vp9 -crf 31 -b:v 0 -b:a 192k -cpu-used 5 `
-    -pix_fmt yuva420p $outputVids\$shortName-$suffix.webm
+    -pix_fmt yuva420p $outFile
 } elseif ($inputCodec -eq "vp8") {
     write-host "vp8, ensuring correct decoder" -ForegroundColor Yellow
     ffmpeg -$ow -loglevel $ll -c:v libvpx -i "$inputStinger" `
     -c:v libvpx-vp9 -crf 31 -b:v 0 -b:a 192k -cpu-used 5 `
-    -pix_fmt yuva420p $outputVids\$shortName-$suffix.webm
+    -pix_fmt yuva420p $outFile
 } else {
     Write-Host "not a webm, trying auto" -ForegroundColor Yellow
     ffmpeg -$ow -loglevel $ll -i "$inputStinger" `
     -c:v libvpx-vp9 -crf 31 -b:v 0 -b:a 192k -cpu-used 5 `
-    -pix_fmt yuva420p $outputVids\$shortName-$suffix.webm
+    -pix_fmt yuva420p $outFile
 }
 
 $endTime = get-date
@@ -62,9 +60,8 @@ $time = new-timespan -start $startTime -End $endTime
 
 write-host "`r"
 Write-host "Done! Completed in: $time" -ForegroundColor Magenta
-Write-Host "Please test $outputVids\$shortName-$suffix.webm" -ForegroundColor Green
+Write-Host "Please test $outfile" -ForegroundColor Green
 
-Pop-Location #pop location twice to return you to
-Pop-Location #the working dir it was ran from
+Pop-Location
 psPause
 exit 0
